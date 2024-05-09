087bea8276adf32fea1b261ab67d90664a39ac0c
joetraining1 <josephbagussw@gmail.com>
2024-05-10 03:11:03 +0700

    adding task to be completed

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -13,6 +13,19 @@ const Index = () => {
                     value={"Halaman Home Aplikasi Arsip dan Purchasing"}
                 />
             </BoxContainer>
+            <BoxContainer>
+                <SectionHeader title={"Things Needed To be Done"} />
+                <ul>
+                    <li>1. Complete Archiving Feature</li>
+                    <li>2. Complete User Feature</li>
+                    <li>3. Complete Purchase Order Feature</li>
+                    <li>4. Complete Master Data</li>
+                    <li>5. Table Column</li>
+                    <li>6. Feature Sequence</li>
+                    <li>7. Auth</li>
+                    <li>8. Guest Page</li>
+                </ul>
+            </BoxContainer>
         </PageContainer>
     );
 };
